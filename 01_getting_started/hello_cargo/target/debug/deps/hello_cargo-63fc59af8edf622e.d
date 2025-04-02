102ba71e1fe220243e32043e0bde969aa9a1e2cc
yuki-miyakawa <127266099+yuki-miyakawa@users.noreply.github.com>
2025-04-02 14:08:03 +0000

    getting started

---
+++ 01_getting_started/hello_cargo/target/debug/deps/hello_cargo-63fc59af8edf622e.d
@@ -0,0 +1,5 @@
+/Users/yuki/src/my-repository/rustacean/hello_cargo/target/debug/deps/hello_cargo-63fc59af8edf622e: src/main.rs
+
+/Users/yuki/src/my-repository/rustacean/hello_cargo/target/debug/deps/hello_cargo-63fc59af8edf622e.d: src/main.rs
+
+src/main.rs:
---
+++ 01_getting_started/hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/Users/yuki/src/my-repository/rustacean/hello_cargo/target/debug/hello_cargo: /Users/yuki/src/my-repository/rustacean/hello_cargo/src/main.rs
