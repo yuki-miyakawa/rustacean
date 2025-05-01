2ce04abd62301b4d383da0caaad6199860af7fa7
yuki-miyakawa <yuki.miyakawa@finatext.com>
2025-05-02 07:02:23 +0900

    rename

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -1,6 +1,4 @@
 fn main() {
-    let s1 = String::from("hello");
-    let s2 = String::from("こんにちは")
-    println!("{}", s2);
-    println!("{}", s1);
+    let b1 = b' ';
+    println!("{}", b1);
 }
