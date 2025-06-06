51b6a0b109ef896c262741a659a3effa556ca058
yuki-miyakawa <yuki.miyakawa@finatext.com>
2025-06-06 10:07:18 +0900

    chore

---
+++ 07_restaurant/target/debug/deps/restaurant-087528bb3bb3f623.d
@@ -0,0 +1,6 @@
+/Users/yuki.miyakawa/src/my-repository/rustacean/07_restaurant/target/debug/deps/librestaurant-087528bb3bb3f623.rmeta: src/lib.rs src/front_of_house.rs
+
+/Users/yuki.miyakawa/src/my-repository/rustacean/07_restaurant/target/debug/deps/restaurant-087528bb3bb3f623.d: src/lib.rs src/front_of_house.rs
+
+src/lib.rs:
+src/front_of_house.rs:
--- sandbox/src/main.rs
+++ sandbox/src/main.rs
@@ -1,21 +1,8 @@
-struct User {
-    name: String,
-    age: u32,
-}
-
-impl User {
-    fn print_name(&mut self) {
-        self.name = "John".to_string();
-        println!("{}", self.name);
-        println!("{}", self.age);
-    }
-}
-
 fn main() {
-    let  user = User {
-        name: "taro".to_string(),
-        age: 20,
-    };
+    let mut v = Vec::new();
 
-    user.print_name();
+    v.push(111111111111111111);
+    v.push(6);
+    v.push(7);
+    v.push(8);
 }
